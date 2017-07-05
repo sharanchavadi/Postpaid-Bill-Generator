84192e2e414ac050a59d76db7015a2efa9aa7700
Sharan Chavadi <sharanchavadi@Ashwinis-MacBook-Pro.local>
2017-07-05 13:14:17 +0530

    index is done

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,10 +1,17 @@
 class ReportController < ApplicationController
+
   def index
+  	@contacts = Contact.all
+  	@logs = current_user.logs
+  	@users = User.all
   end
 
   def logs
+   
   end
 
   def contacts
+    
   end
+
 end
